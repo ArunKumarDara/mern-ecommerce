0d588f051d7d863e36a54073e93c3bf0c77e13fa
Arun Kumar <mymailarun32@gmail.com>
2024-01-18 01:24:04 +0530

    feat: Create required pages, add routes, and integrate links into elements

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import LoginSignup from "./Pages/LoginSignup";
+import Cart from "./Pages/Cart";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />} />
+          <Route path="/men" element={<ShopCategory category="men" />} />
+          <Route path="/women" element={<ShopCategory category="women" />} />
+          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route path="/products" element={<Product />}>
+            <Route path=":productId" element={Product} />
+          </Route>
+          <Route path="/login" element={<LoginSignup type="login" />} />
+          <Route path="/signup" element={<LoginSignup type="signup" />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
